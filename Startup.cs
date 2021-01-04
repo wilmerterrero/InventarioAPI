3fe542724cfffcebc874f55f1f9b1f561c135aba
wilmerterrero <wilmerterrero1922@gmail.com>
2021-01-03 22:21:46 -0400

    adding connectionString in production

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using AutoMapper;
 using InventarioAPI.Context;
 using InventarioAPI.Entities;
@@ -11,14 +9,12 @@ using InventarioAPI.Models;
 using InventarioAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 /*
